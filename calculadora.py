7114bec153c76bd3dd377a9dfdb0ac9e27c71b78
miriancwb1 <163296336+miriancwb1@users.noreply.github.com>
2024-03-13 08:38:44 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+num1 = float(input("Digite o primeiro numero "))
+num2 = float(input("Digite o segundo numero "))
+
+
+soma = num1 + num2
+sub= num1  - num2
+mul = num1 * num2
+div = num1 / num2
+
+
+print("A soma é ", soma)
+print("A subtração é ", sub)
+print("A multiplicação é ", mul)
+print("A divisão é ", div)
