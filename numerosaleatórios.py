cf1061503d8a42e7d0f05fb3b3bf97d5028078eb
miriancwb1 <163296336+miriancwb1@users.noreply.github.com>
2024-03-18 12:00:44 -0300

    Add files via upload

---
+++ numerosaleatórios.py
@@ -0,0 +1,7 @@
+import random
+numero_minímo = int(input("digite o valor minímo"))
+numero_maximo = int(input("digite o valor máximo"))
+
+numero_aleatório = random.randint (numero_minímo,numero_maximo)
+print("o numero aleatorio é", numero_aleatório)
+
